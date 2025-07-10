4e5db07047e482342f3b1387b7b9ae3dfd432b3a
ivedudec <153448724+ivedudec@users.noreply.github.com>
2025-07-10 02:40:17 -0600

    Create snap-update-alert.stories.js

---
+++ ui/components/app/snaps/snap-update-alert/snap-update-alert.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SnapUpdateAlert from '.';
+
+export default {
+  title: 'Components/App/snaps/SnapUpdateAlert',
+  component: SnapUpdateAlert,
+  argTypes: {
+    onUpdateClick: { action: 'onUpdateClick' },
+  },
+};
+
+export const DefaultStory = args => <SnapUpdateAlert {...args} />;
+DefaultStory.storyName = 'Default';
+DefaultStory.args = {};
